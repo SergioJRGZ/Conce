a6c3ac98dc2befe82a9cb205e049c0c21c35f322
SergioJRGZ <sergiogar0401@gmail.com>
2025-03-03 11:13:14 +0100

    Update usuarios.php
    Página principal para manejar usuarios

--- usuarios.php
+++ usuarios.php
@@ -1,15 +1,3 @@
-<?php
-session_start();
-$host = "localhost";
-$user = "root";
-$pass = "rootroot";
-$db = "concesionario";
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -17,14 +5,16 @@ if ($conn->connect_error) {
     <title>Gestión de Usuarios</title>
 </head>
 <body>
-    <h1>Gestión de Usuarios</h1>
-    <ul>
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="usuarios_añadir.php">Añadir</a></li>
-        <li><a href="usuarios_listar.php">Listar</a></li>
-        <li><a href="usuarios_buscar.php">Buscar</a></li>
-        <li><a href="usuarios_modificar.php">Modificar</a></li>
-        <li><a href="usuarios_borrar.php">Borrar</a></li>
-    </ul>
+
+<h2>Gestión de Usuarios</h2>
+
+<ul>
+    <li><a href="añadir_usuario.php">Añadir Usuario</a></li>
+    <li><a href="listar_usuarios.php">Listar Usuarios</a></li>
+    <li><a href="buscar_usuario.php">Buscar Usuario</a></li>
+</ul>
+
+<a href="index.php">Volver al inicio</a>
+
 </body>
 </html>
